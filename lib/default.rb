879b7e42e7862c8975fe7869db00833d627686b4
yazgoo <yazgoo@gmail.com>
2017-12-01 20:53:56 +0100

    stuff

--- lib/default.rb
+++ lib/default.rb
@@ -1,12 +1,11 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
-include Nanoc3::Helpers::Blogging
-include Nanoc3::Helpers::Tagging
-include Nanoc3::Helpers::Rendering
-include Nanoc3::Helpers::LinkTo
+include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::Tagging
+include Nanoc::Helpers::Rendering
+include Nanoc::Helpers::LinkTo
 
 module PostHelper
-
   def get_post_date(post)
     attribute_to_time(post[:created_at])
   end
@@ -15,19 +14,18 @@ module PostHelper
     get_post_date(post).strftime('%B %-d, %Y')
   end
 
-  def relative_path_to_index what
-      relative_path_to(what) + "index.html"
+  def relative_path_to_index(what)
+    relative_path_to(what) + "index.html"
   end
 
   def get_post_start(post)
     content = post.compiled_content
     if content =~ /\s<!-- more -->\s/
       content = content.partition('<!-- more -->').first +
-      "<div class='read-more pull-right'><a href='#{relative_path_to_index(post)}'>Continue reading &rsaquo;</a></div>"
+                "<div class='read-more pull-right'><a href='#{relative_path_to_index(post)}'>Continue reading &rsaquo;</a></div>"
     end
     return content
   end
-
 end
 
 include PostHelper
